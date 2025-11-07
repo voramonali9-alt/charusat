87888c57dabab6f622e8127e6b18e3f51b9e843d
voramonali9-alt <voramonali9@gmail.com>
2025-11-07 12:06:28 +0530

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-    int arr[10],i,asum,esum,missing;
+    int arr[10],n,i,asum,esum,missing;
     asum=0;
     printf("enter the size of the array");
     scanf("%d",&n);
@@ -10,15 +10,17 @@ int main(){
         printf("enter arr[%d]=",i);
         scanf("%d",&arr[i]);
         asum=asum+arr[i];
-     printf("asum=%d",asum);
-      esum=(n*(n+1))/2;
-      printf("%d",esum);
-        missing=esum-asum;
-        printf("missing=%d",missing);
+    
     }
+    esum=(n*(n+1))/2;
+    missing=esum-asum;
+    
     printf("the content of array is:\n");
-   /for(int i=0;i<n;i++){
+   for(int i=0;i<n-1;i++){
        printf("arr[%d]=%d\n",i,arr[i]);
     }
+    printf("%d",esum);
+    printf("%d",asum);
+    printf("%d",missing);
     return 0;
 }
