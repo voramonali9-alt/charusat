be295a957c3596ba1cc0592b1f3bd56f7becb6af
voramonali9-alt <voramonali9@gmail.com>
2025-11-07 12:56:42 +0530

    Update pattern 4.c

--- pattern 4.c
+++ pattern 4.c
@@ -4,19 +4,22 @@ int n=4;
 for(int i=1;i<=n;i++){
 for(int s=1;s<=n-i;s++)
 printf(" ");
-for(int j=1;j<=i;j++){
-printf("%c",'A'+j-1);}
-printf("\n");}
-for(int i=n-1;i>=1;i--){
-for(int s=1;s<n-i;s++){
-printf(" ");}
-for(int j=1;j<=i;j++){
+for(int j=1;j<=i;j++)
 printf("%c",'A'+j-1);
-printf("\n");}
-for(int j=i-1;j>=1;j--){
-printf("%c",'A'+j-1);}
-printf("\n");}
+printf("\n");
+}
+for(int i=n-1;i>=1;i--){
+for(int s=1;s<=n-i;s++)
+printf(" ");
+for(int j=1;j<=i;j++)
+printf("%c",'A'+j-1);
+printf("\n");
+for(int j=i-1;j>=1;j--)
+printf("%c",'A'+j-1);
+printf("\n");
+}
 
 
 return 0;
 }
+
