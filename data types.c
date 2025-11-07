45d61616e8e649c8e36bac3caad9ad2eee127d0b
voramonali9-alt <voramonali9@gmail.com>
2025-11-07 12:23:04 +0530

    Refactor data types program for better clarity

--- data types.c
+++ data types.c
@@ -1,15 +1,21 @@
-#include<stdio.h>
-#include<limits.h>
-#include<float.h>
-int main(){
-printf("char size=%z ,min= %d,max= %d",sizeof(char),CHAR_MIN,CHAR_MAX);
-printf("unsigned char size=%z,min=%d,max=%u \n",sizeof(unsigned char),0,UCHAR_MAX);
-printf("char size=%z ,min= %d,max= %d",sizeof(char),CHAR_MIN,CHAR_MAX);
-printf("unsigned char size=%z,min=%d,max=%u \n",sizeof(unsigned char),0,UCHAR_MAX);
-printf("char size=%z ,min= %d,max= %d",sizeof(char),CHAR_MIN,CHAR_MAX);
-printf("unsigned char size=%z,min=%d,max=%u \n",sizeof(unsigned char),0,UCHAR_MAX);
-printf("char size=%z ,min= %d,max= %d",sizeof(char),CHAR_MIN,CHAR_MAX);
-printf("unsigned char size=%z,min=%d,max=%u \n",sizeof(unsigned char),0,UCHAR_MAX);
-printf("char size=%z ,min= %d,max= %d",sizeof9(char),CHAR_MIN,CHAR_MAX);
-printf("unsigned char size=%z,min=%d,max=%u \n",sizeof(unsigned char),0,UCHAR_MAX);
-return 0;}
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main(void) {
+    printf("char size = %zu, min = %d, max = %d\n", sizeof(char), CHAR_MIN, CHAR_MAX);
+    printf("unsigned char size = %zu, min = %d, max = %u\n", sizeof(unsigned char), 0, UCHAR_MAX);
+  printf("short size = %zu, min = %d, max = %d\n", sizeof(short), SHRT_MIN, SHRT_MAX);
+    printf("unsigned short size = %zu, min = %d, max = %u\n", sizeof(unsigned short), 0, USHRT_MAX);
+  printf("int size = %zu, min = %d, max = %d\n", sizeof(int), INT_MIN, INT_MAX);
+    printf("unsigned int size = %zu, min = %d, max = %u\n", sizeof(unsigned int), 0, UINT_MAX);
+printf("long size = %zu, min = %ld, max = %ld\n", sizeof(long), LONG_MIN, LONG_MAX);
+   printf("unsigned long size = %zu, min = %d, max = %lu\n", sizeof(unsigned long), 0, ULONG_MAX);
+ printf("float size = %zu, min = %e, max = %e\n", sizeof(float), FLT_MIN, FLT_MAX);
+    printf("double size = %zu, min = %e, max = %e\n", sizeof(double), DBL_MIN, DBL_MAX);
+    printf("long double size = %zu, min = %Le, max = %Le\n", sizeof(long double), LDBL_MIN, LDBL_MAX);
+
+    return 0;
+}
+
+
