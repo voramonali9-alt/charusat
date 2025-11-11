77adfd8b18b7453f4372c7afa73fcca4bff0bc41
voramonali9-alt <voramonali9@gmail.com>
2025-11-11 20:47:44 +0530

    Create 20.c

---
+++ 20.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+  
+    scanf("%d", &n);
+    
+    int ids[n - 1];
+    printf("Enter %d participant IDs:\n", N - 1);
+    
+    int sum = 0, totalSum = N * (N + 1) / 2;
+    
+    for (int i = 0; i < N - 1; i++) {
+        scanf("%d", &ids[i]);
+        sum += ids[i];
+    }
+    
+    int missingID = totalSum - sum;
+    printf("Missing Participant ID: %d\n", missingID);
+    
+    return 0;
+}
