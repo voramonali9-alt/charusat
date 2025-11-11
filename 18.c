cae6c2786cc3f545eccaa067bdda720e9509705c
voramonali9-alt <voramonali9@gmail.com>
2025-11-11 11:41:28 +0530

    Create 18.c

---
+++ 18.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n <= 0) {
+        printf("Invalid input\n");
+        return 0;
+    }
+    for (i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+    return 0;
+}
