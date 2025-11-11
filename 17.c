67a5ea18bd21c8e7d1bfd682c5258351ae946556
voramonali9-alt <voramonali9@gmail.com>
2025-11-11 11:40:24 +0530

    Create 17.c

---
+++ 17.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    float distance = 0;
+    int minute = 0;
+    while (1) {
+        minute++;
+        distance += 0.5;
+        printf("Minute %d: Distance covered = %.1f km\n", minute, distance);
+        if (distance >= 10.0)
+            break;
+    }
+    printf("Marathon complete!\n");
+    return 0;
+}
