186efc652269ca664a5b3b425e42fff2482281b9
voramonali9-alt <voramonali9@gmail.com>
2025-11-11 08:13:53 +0530

    Update entryticket.c

--- entryticket.c
+++ entryticket.c
@@ -2,24 +2,23 @@
 
 int main() {
     int age;
-    const int TICKET_PRICE = 200; // constant fare for adults
+     int Price = 200; 
 
     printf("Enter visitor's age: ");
     scanf("%d", &age);
-
-    // validate age
     if(age < 0 || age > 120) {
-        printf("Error: Invalid age entered!\n");
+        printf("Invalid age entered!\n");
     }
     else if(age < 12) {
-        printf("Child (Age %d) → Free Entry ✅\n", age);
+        printf("Child (Age %d)  Free Entry \n", age);
     }
     else if(age >= 60) {
-        printf("Senior Citizen (Age %d) → Free Entry ✅\n", age);
+        printf("Senior Citizen (Age %d) Free Entry \n", age);
     }
     else {
-        printf("Adult (Age %d) → Ticket Price = ₹%d\n", age, TICKET_PRICE);
+        printf("Adult (Age %d)  Ticket Price = ₹%d\n", age, price);
     }
 
     return 0;
 }
+
