68f8cb7304ad925d78f57dd051ac6c9a563b7922
voramonali9-alt <voramonali9@gmail.com>
2025-11-11 21:31:55 +0530

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+void generateSavings(int n) {
+    long long first = 1, second = 1, next;
+    long long total = 0;
+
+    printf("\n Savings Growth Pattern:\n");
+    if (n <= 0) {
+        printf("Invalid number of months! Please enter n ≥ 1.\n");
+        return;
+    }
+if (n >= 1) {
+        printf("Month 1: ₹%lld\n", first);
+        total += first;
+    }    if (n >= 2) {
+        printf("Month 2: ₹%lld\n", second);
+        total += second;
+    }
+    for (int i = 3; i <= n; i++) {
+        next = first + second;
+        printf("Month %d: ₹%lld\n", i, next);
+        total += next;
+        first = second;
+        second = next;
+    }
+    printf("\n Total Savings after %d months: ₹%lld\n", n, total);
+}
+
+int main() {
+    int n;
+printf("Enter the number of months: ");
+    scanf("%d", &n);
+    generateSavings(n);
+
+    return 0;
+}
