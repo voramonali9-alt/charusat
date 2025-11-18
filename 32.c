63effd8df78ac53c9ff9cf37ed098768171f4e7c
voramonali9-alt <voramonali9@gmail.com>
2025-11-18 13:56:10 +0530

    Update 32.c

--- 32.c
+++ 32.c
@@ -1,6 +1,6 @@
+//monali vora 25ce140
 #include <stdio.h>
 #include <string.h>
-
 void reverse(char word[]) {
     int i, j;
     char temp;
@@ -8,27 +8,21 @@ void reverse(char word[]) {
     for(i = 0; i < j; i++, j--) {
         temp = word[i];
         word[i] = word[j];
-        word[j] = temp;
-    }
-}
-
+        word[j] = temp;  }}
 int main() {
     FILE *f1, *f2;
     char word[100];
-
-    f1 = fopen("input.txt", "r");
+ f1 = fopen("input.txt", "r");
     f2 = fopen("output.txt", "w");
   if(f1 == NULL || f2 == NULL) {
         printf("File not found!\n");
-        return 1;
-    }
+        return 1;}
  while(fscanf(f1, "%s", word) != EOF) {
         reverse(word);
         fprintf(f2, "%s ", word);
     }
 printf("All words reversed\n");
-
-    fclose(f1);
-    fclose(f2);
+fclose(f1);
+ fclose(f2);
   return 0;
 }
