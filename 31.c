3a85e3e3665614817f7828fc38957a653b5f1142
voramonali9-alt <voramonali9@gmail.com>
2025-11-18 13:54:27 +0530

    Update 31.c

--- 31.c
+++ 31.c
@@ -1,46 +1,24 @@
+//monali vora 25ce140
 #include <stdio.h>
 #include <stdlib.h>
-
 int main() {
     char *summary;
     int initial_size, new_size;
-
-   
-    scanf("%d", &initial_size);
+ scanf("%d", &initial_size);
     getchar(); 
-  
-    summary = (char *)calloc(initial_size, sizeof(char));
-
-    if (summary == NULL) {
-      
-        return 1;
-    }
-
-   
+   summary = (char *)calloc(initial_size, sizeof(char));
+ if (summary == NULL) {
+     return 1;  }
     fgets(summary, initial_size, stdin);
-
-    printf("\nInitial Summary:\n%s", summary);
-
-   
-  
+ printf("\nInitial Summary:\n%s", summary);
     scanf("%d", &new_size);
     getchar(); 
-
-    summary = (char *)realloc(summary, new_size * sizeof(char));
-
-    if (summary == NULL) {
+summary = (char *)realloc(summary, new_size * sizeof(char));
+ if (summary == NULL) {
         printf("Memory reallocation failed!\n");
-        return 1;
-    }
-
-  
-    fgets(summary, new_size, stdin);
-
-    printf("\nUpdated Article Summary:\n%s", summary);
-
-   
-    free(summary);
-
-   
+        return 1;}
+ fgets(summary, new_size, stdin);
+ printf("\nUpdated Article Summary:\n%s", summary);
+free(summary);
     return 0;
 }
