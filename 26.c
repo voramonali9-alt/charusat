6fcbbc435923d919e21b95cd4c09cce45aedd897
voramonali9-alt <voramonali9@gmail.com>
2025-11-18 13:34:48 +0530

    Update 26.c

--- 26.c
+++ 26.c
@@ -1,14 +1,11 @@
+//monali vora 25ce140
 #include <stdio.h>
 #include <math.h>
-
-
-void inputSides(float *a, float *b, float *c) {
+void inputsides(float *a, float *b, float *c) {
     printf("Enter the lengths of the three sides of the triangle:\n");
     scanf("%f %f %f", a, b, c);
 }
-
-
-int isValidTriangle(float a, float b, float c) {
+int isValidtriangle(float a, float b, float c) {
     if (a <= 0 || b <= 0 || c <= 0) {
         printf(" Error: Side lengths must be positive values.\n");
         return 0;
@@ -18,54 +15,35 @@ int isValidTriangle(float a, float b, float c) {
     else
         return 0;
 }
-
-
-float calculateArea(float a, float b, float c) {
-    float s = (a + b + c) / 2; // semi-perimeter
+float calculatearea(float a, float b, float c) {
+    float s = (a + b + c) / 2; 
     float area = sqrt(s * (s - a) * (s - b) * (s - c));
     return area;
 }
-
-
-void classifyTriangle(float a, float b, float c) {
+void classifytriangle(float a, float b, float c) {
     if (a == b && b == c)
-        printf("🔹 Triangle Type: Equilateral\n");
+        printf(" equilateral\n");
     else if (a == b || b == c || a == c)
-        printf(" Triangle Type: Isosceles\n");
+        printf("  isosceles\n");
     else
-        printf(" Triangle Type: Scalene\n");
+        printf(" scalene\n");
 }
-
-
-float calculatePerimeter(float a, float b, float c) {
+float calculateperimeter(float a, float b, float c) {
     return a + b + c;
 }
 
 
 int main() {
     float a, b, c;
-
- 
-    inputSides(&a, &b, &c);
-
-    
-    if (isValidTriangle(a, b, c)) {
-        printf(" The given sides form a valid triangle.\n");
-
-       
-        classifyTriangle(a, b, c);
-
-       
-        float area = calculateArea(a, b, c);
-        printf("Area of the triangle: %.2f square units\n", area);
-
-       
-        float perimeter = calculatePerimeter(a, b, c);
-        printf(" Perimeter of the triangle: %.2f units\n", perimeter);
-    } 
+ inputsides(&a, &b, &c);
+ if (isValidtriangle(a, b, c)) {
+        printf("  valid triangle.\n");
+classifytriangle(a, b, c);
+float area = calculatearea(a, b, c);
+        printf("Area: %.2f square units\n", area);
+  float perimeter = calculateperimeter(a, b, c);
+        printf(" Perimeter: %.2f units\n", perimeter); } 
     else {
         printf(" The given sides do not form a valid triangle.\n");
-    }
-
-    return 0;
+    }  return 0;
 }
