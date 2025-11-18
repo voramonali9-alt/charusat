9f5f680130c8fd2d9cacc748954beefb02621021
voramonali9-alt <voramonali9@gmail.com>
2025-11-18 13:27:20 +0530

    Update 24 copy.c

--- 24 copy.c
+++ 24 copy.c
@@ -1,36 +1,28 @@
+//monali vora 25ce140
 #include <stdio.h>
 int noteLength(char note[]) {
     int i = 0;
     while (note[i] != '\0') i++;
     return i;
-}
-
-void reverse(char note[]) {
+}void reverse(char note[]) {
     int len = noteLength(note);
     for (int i = 0, j = len - 1; i < j; i++, j--) {
         char temp = note[i];
         note[i] = note[j];
-        note[j] = temp;
-    }
-}
-
+        note[j] = temp; }}
 int compare(char a[], char b[]) {
     int i = 0;
     while (a[i] != '\0' && b[i] != '\0') {
         if (a[i] != b[i]) return a[i] - b[i];
-        i++;
-    }
-    return a[i] - b[i];
-}
+        i++;}
+    return a[i] - b[i];}
 
 void copy(char src[], char dest[]) {
     int i = 0;
     while (src[i] != '\0') {
         dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-}
+        i++;   }
+    dest[i] = '\0';}
 void concatenate(char a[], char b[]) {
     int i = 0, j = 0;
     while (a[i] != '\0') i++;
@@ -43,15 +35,11 @@ void concatenate(char a[], char b[]) {
 void toUpper(char note[]) {
     for (int i = 0; note[i] != '\0'; i++) {
         if (note[i] >= 'a' && note[i] <= 'z')
-            note[i] = note[i] - 32;
-    }
-}
+            note[i] = note[i] - 32; }}
 void toLower(char note[]) {
     for (int i = 0; note[i] != '\0'; i++) {
         if (note[i] >= 'A' && note[i] <= 'Z')
-            note[i] = note[i] + 32;
-    }
-}
+            note[i] = note[i] + 32; }}
 void capitalizeWords(char note[]) {
     int capNext = 1;
     for (int i = 0; note[i] != '\0'; i++) {
@@ -61,31 +49,23 @@ void capitalizeWords(char note[]) {
             note[i] = note[i] - 32;
             capNext = 0;
         } else {
-            capNext = 0;
-        }
-    }
-}
+            capNext = 0; }  }}
 
 int main() {
     char note[1000] = "";
     char temp[1000];
     int choice;
-
     printf("Enter your initial note: ");
     fgets(note, 1000, stdin);
-
-    int len = noteLength(note);
+int len = noteLength(note);
     if (note[len - 1] == '\n') note[len - 1] = '\0';
-
-    do {
-        printf("\n--- Note Manager ---\n");
+    do { printf("\n--- Note Manager ---\n");
         printf("1. Length\n2. Reverse\n3. Compare\n4. Copy\n5. Concatenate\n");
         printf("6. Upper Case\n7. Lower Case\n8. Capitalize Words\n0. Exit\n");
         printf("Enter choice: ");
         scanf("%d", &choice);
         getchar();
-
-        switch (choice) {
+  switch (choice) {
             case 1:
                 printf("Length: %d\n", noteLength(note));
                 break;
@@ -132,9 +112,7 @@ int main() {
                 break;
             default:
                 printf("Invalid choice.\n");
-        }
-    } while (choice != 0);
+        }  } while (choice != 0);
+    return 0;}
 
-    return 0;
-}
 
